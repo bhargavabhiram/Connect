8a2557320eb81bf46c89199ccf7bff1dd96db23b
bhargavabhiram <bhargavabhiram66@gmail.com>
2024-03-16 14:45:29 +0530

    Login page Test

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import MyNavBar from './MyNavBar';
 
 function Welcome() {
 
     return (
 
-        <div>
-
-        Welcome
-
-        </div>
+        <MyNavBar />
 
 
     );
